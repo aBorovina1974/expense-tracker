fe0f568ea35d71cb393b05c28842c4fb163014b5
aBorovina1974 <aborovina74@gmail.com>
2023-01-10 22:03:54 +0100

    Rendering lists & conditional content

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
 function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+  const [expenses, setNewExpenses] = useState(DUMMY_EXPENSES);
+  const addExpenseHandler = (expense) => {
+    //If we depend on previous state we should
+    //pass function as argument to state updating function
+    //which will receive previous state automatically
+    setNewExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
 
   //Alternative syntax to JSX code
   // return React.createElement(
@@ -33,7 +44,7 @@ function App() {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
